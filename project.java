3b26361aaa1c6c53f8051eafc1baa91b920e3876
Ayush Kumar <ayushhh.1106@gmail.com>
2026-04-25 10:12:32 +0530

    Fix typo in food item name from 'Burger' to 'Burgerr'

--- project.java
+++ project.java
@@ -93,7 +93,7 @@ public class project {
         Scanner sc = new Scanner(System.in);
 
         FoodItem[] menu = {
-                new FoodItem("Burger", 100),
+                new FoodItem("Burgerr", 100),
                 new FoodItem("Pizza", 300),
                 new FoodItem("Pasta", 200),
                 new FoodItem("Sandwich", 150)
