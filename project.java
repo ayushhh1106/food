06edf37c804d4226684903cfce56da3dc7a09c43
Ayush Kumar <ayushhh.1106@gmail.com>
2026-05-23 16:50:19 +0530

    Update project.java

--- project.java
+++ project.java
@@ -96,7 +96,7 @@ public class project {
                 new FoodItem("Burgerr", 150),
                 new FoodItem("puri", 300),
                 new FoodItem("paneer", 250),
-                new FoodItem("momos", 150)
+                new FoodItem("momo", 150)
         };
 
         Restaurant restaurant = new Restaurant("Food Hub", menu);
